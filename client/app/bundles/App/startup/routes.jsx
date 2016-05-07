1bda2478d96eb3e9b0ad7dd734464928a25c29b4
Sflaton <suzanne.flaton@gmail.com>
2016-05-07 11:41:10 +0200

    userseeds work

--- client/app/bundles/App/startup/routes.jsx
+++ client/app/bundles/App/startup/routes.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import App from '../containers/App';
+import AppWidget from '../components/AppWidget';
 import Home from '../containers/Home';
 import Test from '../containers/Test';
 
 export default (
   <Route path="ui" component={App}>
 
-    <IndexRoute component={Home} />
+    <IndexRoute component={AppWidget} />
     <Route path="test" component={Test} />
 
   </Route>
