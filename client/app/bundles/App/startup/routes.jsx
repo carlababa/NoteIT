de0cdb09d9aec03e10690a3350b6610fb719d5b4
Carla Babadopulos <cacababa@gmail.com>
2016-05-12 11:29:38 +0200

    Changed index route to /

--- client/app/bundles/App/startup/routes.jsx
+++ client/app/bundles/App/startup/routes.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { Route, IndexRoute } from 'react-router';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 import App from '../containers/App';
 import AppWidget from '../components/AppWidget';
 import Home from '../containers/Home';
 import Test from '../containers/Test';
 
-export default (
-  <Route path="ui" component={App}>
-
-    <IndexRoute component={AppWidget} />
-    <Route path="test" component={Test} />
-
-  </Route>
+export default (props) => (
+  <Router history={browserHistory} {...props} >
+    <Route path="/" component={App}>
+      <IndexRoute component={AppWidget} />
+      <Route path="test" component={Test} />
+    </Route>
+  </Router>
 );
